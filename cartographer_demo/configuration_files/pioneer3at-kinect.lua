8802e1afbbde5c6114e8f30f328cc70fe052e68e
hogashikame <62817697+hogashikame@users.noreply.github.com>
2020-09-03 20:10:58 +0200

    Code format
    Format the code

--- cartographer_demo/configuration_files/pioneer3at-kinect.lua
+++ cartographer_demo/configuration_files/pioneer3at-kinect.lua
@@ -64,48 +64,5 @@ SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 1.
 
 return options
 
---options = {
---  map_builder = MAP_BUILDER,
-  --trajectory_builder = TRAJECTORY_BUILDER,
-  --map_frame = "map",
-  --tracking_frame = "kinect2_ir_link",
-  --published_frame = "odom",
-  --odom_frame = "odom",
-  --provide_odom_frame = false,
-  --use_odometry = true,
-  --use_laser_scan = true,
-  --num_laser_scan = 1,
-  --num_subdivisions_per_laser_scan = 1,
-  --use_multi_echo_laser_scan = false,
-  ---num_point_clouds = 0,
-  --lookup_transform_timeout_sec = 0.2,
-  --submap_publish_period_sec = 0.5,
-  --pose_publish_period_sec = 5e-1,
-  --trajectory_publish_period_sec = 30e-3,
- -- odometry_sampling_ratio = 1.,
---}
 
-
---TRAJECTORY_BUILDER_2D.use_imu_data = false
---T-RAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1e3
---TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.5
---TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = 0.5
-
---SPARSE_POSE_GRAPH.optimize_every_n_scans = 40
-
---TRAJECTORY_BUILDER_2D.min_range = 0.1
---TRAJECTORY_BUILDER_2D.max_range = 5.0
---TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.0
---TRAJECTORY_BUILDER_2D.use_imu_data = false
---TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.3
---TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
---TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(3.)
---TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
---TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
-
---TRAJECTORY_BUILDER_2D.submaps.resolution = 0.035
---TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
---SPARSE_POSE_GRAPH.optimize_every_n_scans = 120
---SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.82
---SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 1.
 
